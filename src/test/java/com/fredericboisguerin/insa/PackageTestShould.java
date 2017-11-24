db79670b27cd87b6cd1e7a5935f59cd4a8f93ba7
tinhte96 <vanthiennhat@gmail.com>
2017-11-24 23:04:18 +0100

    test cost calculation

--- src/test/java/com/fredericboisguerin/insa/PackageTestShould.java
+++ src/test/java/com/fredericboisguerin/insa/PackageTestShould.java
@@ -1,4 +1,22 @@
 package com.fredericboisguerin.insa;
 
+import org.junit.Test;
+
 public class PackageTestShould {
+
+    private int     VALID_HEIGHT    = 229;
+    private int     VALID_WIDTH     = 162;
+    private int     VALID_DEPTH     = 25;
+    private double  VALID_WEIGHT    = 1.8;
+
+    private CountryDestination VN;
+
+    @Test (expected = InvalidDestinationException.class)
+    public void fail_if_destination_is_not_valid()
+        throws InvalidDestinationException{
+
+        Package p = new Package(VALID_HEIGHT, VALID_WIDTH, VALID_DEPTH, VALID_WEIGHT);
+        System.out.println(ShippingCostsCalculator.calculateShippingCost(p,VN));
+    }
+
 }
