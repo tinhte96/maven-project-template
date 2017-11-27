4b5d12ea1921b4da6c536da5f4cfc83aa4d6ee9b
tinhte96 <vanthiennhat@gmail.com>
2017-11-27 21:56:39 +0100

    make it OO, class for small package

---
+++ src/main/java/com/fredericboisguerin/insa/SmallPackage.java
@@ -0,0 +1,15 @@
+package com.fredericboisguerin.insa;
+
+public class SmallPackage extends Package {
+
+    public SmallPackage (int height, int width, int depth, double weight){
+        this.height = height;
+        this.width = width;
+        this.depth = depth;
+        this.weight = weight;
+    }
+
+    public double calculateLocalShippingCost() {
+        return round(12.00);
+    }
+}
