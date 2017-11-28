c341bebeeb90caa1ae68d8350d6c9336a1d976fd
tinhte96 <vanthiennhat@gmail.com>
2017-11-28 16:25:21 +0100

    calcul cost for DOM/TOM

--- src/main/java/com/fredericboisguerin/insa/ShippingCostsCalculator.java
+++ src/main/java/com/fredericboisguerin/insa/ShippingCostsCalculator.java
@@ -5,12 +5,15 @@ public class ShippingCostsCalculator {
     public static double calculateShippingCost(Package p, CountryDestination d) throws InvalidDestinationException {
 
         double standardCost = p.calculateLocalShippingCost();
-        System.out.println(standardCost);
+
         if (d == CountryDestination.FR){
             return standardCost;
         }
         else if (d == CountryDestination.MC){
             return (standardCost * 1.087);
+        }
+        else if (d == CountryDestination.DOMTOM){
+            return (standardCost * 1.054 + 1.26);
         }
         else {
             throw new InvalidDestinationException();
