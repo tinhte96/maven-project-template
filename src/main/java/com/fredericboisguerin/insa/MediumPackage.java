30a0a1806e71a308335e8b9cd5a9aed713b63c8f
tinhte96 <vanthiennhat@gmail.com>
2017-11-29 00:59:52 +0100

    inherit constructor from super class

--- src/main/java/com/fredericboisguerin/insa/MediumPackage.java
+++ src/main/java/com/fredericboisguerin/insa/MediumPackage.java
@@ -2,19 +2,15 @@ package com.fredericboisguerin.insa;
 
 public class MediumPackage extends Package {
 
-    private int height;
-    private int width;
-    private int depth;
-    private double weight;
-
     public MediumPackage (int height, int width, int depth, double weight){
-        this.height = height;
-        this.width = width;
-        this.depth = depth;
-        this.weight = weight;
+        super(height,width,depth,weight);
     }
 
     public double calculateLocalShippingCost() {
         return this.weight * 17.59 + 2.86;
+    }
+
+    public String toString() {
+        return "this is a medium package with cost " + this.roundedCost();
     }
 }
