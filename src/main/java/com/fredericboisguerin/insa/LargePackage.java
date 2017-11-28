0e298f6ba31d049ff14272c12fc1909a64e6030e
tinhte96 <vanthiennhat@gmail.com>
2017-11-29 00:59:09 +0100

    add constructor for Package

--- src/main/java/com/fredericboisguerin/insa/LargePackage.java
+++ src/main/java/com/fredericboisguerin/insa/LargePackage.java
@@ -3,17 +3,17 @@ package com.fredericboisguerin.insa;
 public class LargePackage extends Package {
 
     public LargePackage (int height, int width, int depth, double weight){
-        this.height = height;
-        this.width = width;
-        this.depth = depth;
-        this.weight = weight;
+        super(height,width,depth,weight);
     }
 
     public double calculateLocalShippingCost() {
-
         double volume = this.height * this.width * this.depth * Math.pow(10,-6);
         double costSize = volume * 1.43;
         double costWeight = this.weight * 21.62;
         return Math.max(costSize,costWeight);
+    }
+
+    public String toString() {
+        return "this is a large package with cost " + this.roundedCost();
     }
 }
