99aa73a69f0036b909c0e3757a4eaaf7d2837e17
tinhte96 <vanthiennhat@gmail.com>
2017-11-28 18:04:49 +0100

    main class

---
+++ src/main/java/com/fredericboisguerin/insa/Main.java
@@ -0,0 +1,9 @@
+package com.fredericboisguerin.insa;
+
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+
+}
