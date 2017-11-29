4d406ede038c351514c41b011ca190a5068afd8f
tinhte96 <vanthiennhat@gmail.com>
2017-11-29 01:00:47 +0100

    inherit constructor from package

--- src/main/java/com/fredericboisguerin/insa/SmallPackage.java
+++ src/main/java/com/fredericboisguerin/insa/SmallPackage.java
@@ -2,14 +2,15 @@ package com.fredericboisguerin.insa;
 
 public class SmallPackage extends Package {
 
-    public SmallPackage (int height, int width, int depth, double weight){
-        this.height = height;
-        this.width = width;
-        this.depth = depth;
-        this.weight = weight;
+    public SmallPackage(int height, int width, int depth, double weight) {
+        super(height,width,depth,weight);
     }
 
     public double calculateLocalShippingCost() {
         return 12.00;
+    }
+
+    public String toString() {
+        return "this is a small package with cost " + this.roundedCost();
     }
 }
