7019ae6c1f847b94f1d6cdf7fb7ea2c3c1e89526
tinhte96 <vanthiennhat@gmail.com>
2017-11-29 01:02:52 +0100

    test for askDestination

--- src/test/java/com/fredericboisguerin/insa/UserInterfaceTest.java
+++ src/test/java/com/fredericboisguerin/insa/UserInterfaceTest.java
@@ -1,27 +1,58 @@
 package com.fredericboisguerin.insa;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class UserInterfaceTest {
 
-    @Test
-    public void type_small_package_dimension_return_new_small_package(){
+    private ByteArrayOutputStream out;
 
-        System.setIn(new ByteArrayInputStream("191\n123\n18\n2.354d\n".getBytes()));
+    @Before
+    public void setUp() {
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+    }
+
+    @Test
+    public void call_measure_package(){
+
+        System.setIn(new ByteArrayInputStream("19\n12\n18\n2.354\n".getBytes()));
 
         PackageFactory pf = Mockito.mock(PackageFactory.class);
         UserInterface ui = new UserInterface();
-        ui.askPackageInfo();
+        ui.askPackageInfo(pf);
 
-        int height = 191;
-        int width = 123;
+        int height = 19;
+        int width = 12;
         int depth = 18;
-        double weight = 2.354d;
+        double weight = 2.354;
 
         Mockito.verify(pf).measurePackage(height,width,depth,weight);
     }
+
+    @Test
+    public void destination () throws InvalidDestinationException {
+
+        System.setIn(new ByteArrayInputStream("DOMTOM".getBytes()));
+        UserInterface ui = new UserInterface();
+
+        CountryDestination cd = ui.askDestination();
+        System.out.printf(cd.toString());
+
+        assertThat(standardOutput(), containsString(CountryDestination.DOMTOM.toString()));
+
+    }
+
+    private String standardOutput() {
+        return out.toString();
+    }
+
 }
